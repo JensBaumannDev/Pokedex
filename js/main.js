212ae40ad9b994be4ebb8569680f87700f2ee931
JensBaumannDev <baumannjens@outlook.de>
2026-02-05 22:28:55 +0100

    add loadMore button to show 20 more pokemon every click // add loadingscreen with timeout // set some loadingscreen styles

--- js/main.js
+++ js/main.js
@@ -1,21 +1,4 @@
-let currentLimit = 20;
-
-async function loadPokedex() {
-  initTheme();
-
-  const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=2000");
-  const data = await response.json();
-
-  for (let index = 0; index < currentLimit; index++) {
-    const pokemon = data.results[index];
-
-    const responseDetail = await fetch(pokemon.url);
-    const details = await responseDetail.json();
-
-    renderPokemon(details);
-  }
-}
-
+//#region THEME TOGGLE
 function initTheme() {
   const savedTheme = localStorage.getItem("theme");
   if (savedTheme === "dark") {
@@ -34,4 +17,47 @@ function changeTheme() {
   } else {
     localStorage.setItem("theme", "light");
   }
+}
+//#endregion
+
+let allPokemon = [];
+let currentLimit = 20;
+
+async function loadPokedex() {
+  initTheme();
+
+  const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=2000");
+  const data = await response.json();
+  allPokemon = data.results;
+
+  for (let index = 0; index < currentLimit; index++) {
+    const pokemon = data.results[index];
+
+    const responseDetail = await fetch(pokemon.url);
+    const details = await responseDetail.json();
+
+    renderPokemon(details);
+  }
+  setTimeout(() => {
+    document.getElementById("loader").classList.add("d-none");
+    document.body.classList.remove("noscroll");
+  }, 1000);
+}
+
+async function loadMorePokemon() {
+  document.getElementById("loader").classList.remove("d-none");
+  document.body.classList.add("noscroll");
+
+  for (let index = currentLimit; index < currentLimit + 20; index++) {
+    const pokemon = allPokemon[index];
+    const response = await fetch(pokemon.url);
+    const details = await response.json();
+    renderPokemon(details);
+  }
+  currentLimit += 20;
+
+  setTimeout(() => {
+    document.getElementById("loader").classList.add("d-none");
+    document.body.classList.remove("noscroll");
+  }, 1000);
 }
