8c419ea2adefaa2c72c48ccc2ce343171f929a60
JensBaumannDev <baumannjens@outlook.de>
2026-02-05 21:48:05 +0100

    Add local storage to themeToggle // pokedex will now start with the OS default theme - user can change theme with correct toggle

--- js/main.js
+++ js/main.js
@@ -1,6 +1,8 @@
 let currentLimit = 20;
 
 async function loadPokedex() {
+  initTheme();
+
   const response = await fetch("https://pokeapi.co/api/v2/pokemon?limit=2000");
   const data = await response.json();
 
@@ -14,6 +16,22 @@ async function loadPokedex() {
   }
 }
 
+function initTheme() {
+  const savedTheme = localStorage.getItem("theme");
+  if (savedTheme === "dark") {
+    document.body.classList.add("dark-mode");
+  } else if (savedTheme === "light") {
+    document.body.classList.remove("dark-mode");
+  } else if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+    document.body.classList.add("dark-mode");
+  }
+}
+
 function changeTheme() {
   document.body.classList.toggle("dark-mode");
+  if (document.body.classList.contains("dark-mode")) {
+    localStorage.setItem("theme", "dark");
+  } else {
+    localStorage.setItem("theme", "light");
+  }
 }
